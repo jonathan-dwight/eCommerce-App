0aacf67a0b45454d265c4336911f22a43a5634bb
jonathan-dwight <jd.buendia0711@gmail.com>
2020-12-02 19:41:59 -0800

    Add Header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
 
-import HomePage from './pages/homepage' //pages folder is non reusuable components
+import HomePage from './pages/homepage/homepage' //pages folder is non reusuable components
 import ShopPage from './pages/shop/shop'
+import Header from './components/header/header';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch> 
         <Route exact path ="/shop/" component ={ShopPage} />
         <Route exact path ="/" component={HomePage} />
